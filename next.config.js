57d7835d808aaee86ee0372f9c656a12d20003a6
ethan <ethan@wayougou.com>
2019-08-02 13:10:56 +0800

    update

--- next.config.js
+++ next.config.js
@@ -4,15 +4,9 @@ module.exports = {
     return 'build-last';
   },
   webpack: function (config) {
-    // Unshift polyfills in main entrypoint.
-    const originalEntry = config.entry;
-    config.entry = async () => {
-      const entries = await originalEntry();
-      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-        entries['main.js'].unshift('./polyfills.js');
-      }
-      return entries;
+    config.optimization = {
+      minimize: false
     };
-    return config
+    return config;
   }
 };
